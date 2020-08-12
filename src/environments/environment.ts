2a252837db1cef2025991d506a0457abec6e08a0
shopizer-ecommerce <csamson777@yahoo.com>
2020-08-12 13:02:50 -0400

    backends

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,10 +14,10 @@ export const environment = {
     production: false,
     googleApiKey: '',
     mode: 'MARKETPLACE',
-    shippingapi: 'http://15.223.64.6:9091/api',
     //apiUrl: 'http://aws-demo.shopizer.com:8080/api',
     //apiUrl: 'http://localhost:8080/api',
     apiUrl: 'http://15.223.64.6:9090/api',
+    shippingapi: 'http://15.223.64.6:9091/api',
     client: {
         language: {
             default: 'en',
