155b8b14d14d3bc3cf401d4583943d4d7068f755
shopizer-ecommerce <csamson777@yahoo.com>
2021-03-27 19:53:44 -0400

    Product type adjustments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -18,7 +18,7 @@ export const environment = {
     mode: 'STANDARD',
     //API URL
     apiUrl: 'http://aws-demo.shopizer.com:8080/api',
-    // apiUrl: 'http://localhost:8080/api',
+    //apiUrl: 'http://localhost:8080/api',
     shippingApi: 'http://localhost:8080/shipping/api/v1',
     // shippingApi: 'http://aws-demo.shopizer.com:8080/api',
     client: {
