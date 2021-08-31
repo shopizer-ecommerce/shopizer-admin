74619b1dbeb80a2e70a164d39ef578f777750a87
shopizer-ecommerce <csamson777@yahoo.com>
2021-08-31 12:08:29 -0400

    change backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -17,8 +17,8 @@ export const environment = {
     //MARKETPLACE | BTB | STANDARD
     mode: 'STANDARD',
     //API URL
-    apiUrl: 'http://localhost:8080/api',
-    //apiUrl: 'https://api.perfectopoelesetfoyers.com/api',
+    //apiUrl: 'http://localhost:8080/api',
+    apiUrl: 'https://api.perfectogaz.com/api',
     shippingApi: 'http://localhost:9090/shipping/api/v1',
     client: {
         language: {
